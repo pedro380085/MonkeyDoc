26fa62dc8a6f377b6341226c4943f89d1174d1a6
Pedro Góes <goes@inevent.us>
2015-12-01 21:11:43 -0500

    Initial Commit

---
+++ MonkeyDoc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MonkeyDoc
+//
+//  Created by Pedro Goes on 12/1/15.
+//  Copyright © 2015 Pedro Goes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
